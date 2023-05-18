2ff8243045dbfb2e55008a578db13cd5b0efc59b
YockyMai <g.valera15102003@gmail.com>
2023-05-18 23:38:11 +0500

    Добавление времени для дней при создании услуги

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -10,6 +10,22 @@ const { v4: uuidv4 } = require("uuid");
 const apiError = require("../error/apiError");
 const path = require("path");
 
+const timesData = [
+  { time: "9:00" },
+  { time: "10:00" },
+  { time: "11:00" },
+  { time: "12:00" },
+  { time: "13:00" },
+  { time: "14:00" },
+  { time: "15:00" },
+  { time: "16:00" },
+  { time: "17:00" },
+  { time: "18:00" },
+  { time: "19:00" },
+  { time: "20:00" },
+  { time: "21:00" },
+];
+
 class serviceController {
   async create(req, res, next) {
     try {
